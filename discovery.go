f1b32b8157d989720eebe42933c2ef8dda29e02f
ByronGu <2277165192@qq.com>
2025-03-27 20:32:21 +0800

    added some functions

---
+++ discovery.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"errors"
+	"math"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type SelectMode int
+
+const (
+	RandomSelect     SelectMode = iota // 随机选择
+	RoundRobinSelect                   // 使用随机算法
+)
+
+type Discovery interface {
+	Refresh() error                      // 从注册中心更新服务列表
+	Update(servers []string) error       // 手动更新服务列表
+	Get(mode SelectMode) (string, error) // 根据负载均衡策略选择服务实例
+	GetAll() ([]string, error)           // 返回所有服务实例
+}
+
+type MultiServersDiscovery struct {
+	r       *rand.Rand // 随机数
+	mu      sync.RWMutex
+	servers []string
+	index   int
+}
+
+// NewMultiServerDiscovery 新建服务发现实例
+func NewMultiServerDiscovery(servers []string) *MultiServersDiscovery {
+	d := &MultiServersDiscovery{
+		servers: servers,
+		r:       rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+	d.index = d.r.Intn(math.MaxInt32 - 1)
+	return d
+}
+
+var _ Discovery = (*MultiServersDiscovery)(nil)
+
+func (d *MultiServersDiscovery) Refresh() error {
+	return nil
+}
+
+func (d *MultiServersDiscovery) Update(servers []string) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	d.servers = servers
+	return nil
+}
+
+func (d *MultiServersDiscovery) Get(mode SelectMode) (string, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	n := len(d.servers)
+	if n == 0 {
+		return "", errors.New("rpc discovery: no servers")
+	}
+	switch mode {
+	case RandomSelect:
+		return d.servers[d.r.Intn(n)], nil
+	case RoundRobinSelect:
+		s := d.servers[d.r.Intn(n)%n]
+		d.index = (d.index + 1) % n
+		return s, nil
+	default:
+		return "", errors.New("rpc discovery: unknown mode")
+	}
+}
+
+func (d *MultiServersDiscovery) GetAll() ([]string, error) {
+	d.mu.RLock()
+	defer d.mu.RUnlock()
+	servers := make([]string, len(d.servers), len(d.servers))
+	copy(servers, d.servers)
+	return servers, nil
+}
